50c4009d95342592db1d47ded5c3697f72d5e587
Mohit Adwani <mohitadwani@gmail.com>
2018-04-03 12:08:55 -0500

    added filenames

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import nltk
 from nltk.tokenize import sent_tokenize
+import os
 
-one_essay = open('essays_dataset/essays/38209.txt', 'r').read()
+filenames = os.listdir('essays_dataset/essays/')
+filename = 'essays_dataset/essays/' + filenames[0]
+one_essay = open(filename, 'r').read()
 
 def count_sentences():
     sentences = sent_tokenize(one_essay)
-    print(sentences)
+    for sentence in sentences:
+        print(sentence)
 
 count_sentences()
