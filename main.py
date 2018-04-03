fed8f8664ea31827e13a4b1b2dc6e702e7bc06b2
Mohit Adwani <mohitadwani@gmail.com>
2018-04-03 11:52:47 -0500

    function created

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import nltk
 from nltk.tokenize import sent_tokenize
 
 one_essay = open('essays_dataset/essays/38209.txt', 'r').read()
-sentences = sent_tokenize(one_essay)
-# print(sentences)
-result = ""
-for sentence in sentences:
-	result += "[" + sentence + "] "
-print(result)
+
+def count_sentences():
+    sentences = sent_tokenize(one_essay)
+    print(sentences)
+
+count_sentences()
