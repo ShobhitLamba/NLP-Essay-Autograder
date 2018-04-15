de9279e5f72194088bc3bb33a5d7abc4f9bf4ad7
Shobhit Lamba <shobhitlamba.ynwa@gmail.com>
2018-04-15 15:45:14 -0500

    Modified main.py. Can handle sentences without period between them most of the time.

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-import nltk
-from nltk.tokenize import sent_tokenize
-import os
+# To run sentence count, download Stanford Core NLP package and its python wrapper.
+# I used pycorenlp. You can download it by simply using pip command.
+# To run this code, you need to start a CoreNLP server.
+# Fire up command prompt and traverse to stanford-corenlp-full-2015-12-09 directory
+# or whichever version you downloaded and run:
+# java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
+# Author: Shobhit Lamba, Mohit Adwani
 
-filenames = os.listdir('essays_dataset/essays/')
-filename = 'essays_dataset/essays/' + filenames[0]
-one_essay = open(filename, 'r').read()
+from pycorenlp import StanfordCoreNLP
 
-def count_sentences():
-    sentences = sent_tokenize(one_essay)
-    for sentence in sentences:
-        print(sentence)
+if __name__ == '__main__':
+    nlp = StanfordCoreNLP('http://localhost:9000')
 
-count_sentences()
+    text = ("I am happy I am a king.")
+    output = nlp.annotate(text, properties={
+        'annotators': 'parse',
+        'outputFormat': 'json'
+    })
+
+    count = 0
+
+    for index in range(len(output['sentences'])):
+        parseTree = output['sentences'][0]['parse']
+        parsedList = parseTree.split(' ' or '\r\n')
+        for i, char in enumerate(parseTree):
+            if char == 'S' and parseTree[i - 1] == '(' and parseTree[i + 1] != 'B':
+                count += 1
+
+    print("Number of sentences: ", count)
