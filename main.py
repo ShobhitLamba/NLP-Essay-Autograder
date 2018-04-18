1575da1e96fdcd57e8196c61fa96523f9e0cd5cc
Mohit Adwani <mohitadwani@gmail.com>
2018-04-18 16:21:33 -0500

    verb tense done

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # author: Shobhit Lamba
 
 from mohit_main import count_sentences
-from spelling import spellcheck  
+from spelling import spellcheck
 from grammar import subjectVerbAgreement
-      
+from verb_tense import verb_tense
+
 def final_score(part_a, part_b, part_c_i, part_c_ii):
     #considering part_c_iii, part_d_i and part_d_ii to be zero for part 1 of project
     return 2 * part_a - part_b + part_c_i + part_c_ii
@@ -11,31 +12,37 @@ def final_score(part_a, part_b, part_c_i, part_c_ii):
 if __name__ == '__main__':
     csv_file = open(r'essays_dataset\index.csv', 'r')
     line_index = 0
-    total_essay = [0, 0] 
+    total_essay = [0, 0]
     spellings = []
     grades = [] # 'high' or 'low'
     essay_lengths = []
     sentences = []
     c_i_errors = []
+    part_c_i = 0
+    part_a = 0
+    part_b = 0
+    part_c_ii = 0
     for line in csv_file:
         if line_index != 0:
             line_list = line.split(';')
             filepath = 'essays_dataset/essays/' + line_list[0]
-            
+
             ''' Read file '''
             essay_file = open(filepath, 'r')
             one_essay = essay_file.read()
-            
-            part_a = count_sentences(one_essay)
+
+            part_a, dot_processed_sentences = count_sentences(one_essay)
             part_b = spellcheck(one_essay)
             essay_lengths.append(part_a)
             spellings.append(part_b)
             part_c_i = subjectVerbAgreement(one_essay)
+            # if line_index == 3:
+            part_c_ii = verb_tense(dot_processed_sentences)
 #            for sentence in sentences:
 #                part_c_i += subjectVerbAgreement(sentence)
-            
+
             c_i_errors.append(part_c_i)
-            
+
             if line_list[2].strip().lower() == 'low':
                 # print('low')
                 total_essay[0] += 1
@@ -49,7 +56,11 @@ if __name__ == '__main__':
             if line_index == 1:
                 # break
                 pass
-        
+
+        if line_index == 3:
+            # break
+            pass
+
         line_index += 1
 
     csv_file.close()
---
+++ verb_tense.py
@@ -0,0 +1,137 @@
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
+
+verbs = [
+	"MD",
+	"VB",
+	"VBZ",
+	"VBD",
+	"VBG",
+	"VBN",
+	"VBP",
+    "TO"
+]
+
+verbs_with_adverb = [
+	"MD",
+	"VB",
+	"VBZ",
+	"VBD",
+	"VBG",
+	"VBN",
+	"VBP",
+    "RB"
+]
+
+fourgram_rules = [
+
+"VBZ, RB, VBN, VBN",
+"VBZ, RB, VBN, VBG",
+"MD, RB, VB, VBG",
+"MD, RB, VB, VBN"
+
+]
+
+trigram_rules = [
+
+"VBZ, VBN, VBN",
+"VBZ, VBN, VBG",
+"MD, VB, VBG",
+"MD, VB, VBN",
+
+"VBZ, RB, VBG",
+"VBZ, RB, VBN",
+"VBZ, RB, VB",
+
+"VBP, RB, VBG",
+"VBP, RB, VBN",
+"VBP, RB, VB",
+
+"VBD, RB, VBG",
+"VBD, RB, VBN",
+"VBD, RB, VB",
+
+"MD, RB, VB",
+
+"TO, RB, VB"
+]
+
+bigram_rules = [
+
+"VBZ, VBG",
+"VBZ, VBN",
+"VBZ, VB",
+
+"VBP, VBG",
+"VBP, VBN",
+"VBP, VB",
+
+"VBD, VBG",
+"VBD, VBN",
+"VBD, VB",
+
+"MD, VB",
+
+"TO, VB",
+
+"VBP, RB", #- I am finally fine.
+"VBZ, RB", #- He is finally fine
+"VBD, RB", #- He sang finally.
+
+]
+
+unigram_rules = [
+
+"VBP", #- I am fine.
+"VBZ", #- He is fine
+"VBD", #- He sang.
+"VBG", # - Having some understanding is good
+"TO" # from one to another
+
+]
+
+
+def verb_tense(dot_processed_sentences):
+    verb_tense_errors = 0
+    # print("sssssssssssssssssskdsjfngjkdfngkjdfng")
+    for sentence_index, sentence in enumerate(dot_processed_sentences):
+        pos_tagged_sentence = pos_tag(word_tokenize(sentence))
+        # print(pos_tagged_sentence)
+        tag_sequence = []
+        for tag_index, tag in enumerate(pos_tagged_sentence):
+            if len(tag_sequence) == 0:
+                if tag[1] in verbs:
+                    tag_sequence.append(tag[1])
+            else:
+                if tag[1] in verbs_with_adverb:
+                    tag_sequence.append(tag[1])
+                else:
+                    print(tag_sequence)
+                    if len(tag_sequence) == 4:
+                        if (', ').join(tag_sequence) not in fourgram_rules:
+                            verb_tense_errors += 1
+                            # print(tag, 'not present in fourgram')
+                            # print(pos_tagged_sentence)
+                    elif len(tag_sequence) == 3:
+                        if (', ').join(tag_sequence) not in trigram_rules:
+                            verb_tense_errors += 1
+                            # print(tag, 'not present in trigram')
+                            # print(pos_tagged_sentence)
+                    elif len(tag_sequence) == 2:
+                        if (', ').join(tag_sequence) not in bigram_rules:
+                            verb_tense_errors += 1
+                            # print(tag, 'not present in bigram')
+                            # print(pos_tagged_sentence)
+                    elif len(tag_sequence) == 1:
+                        if tag_sequence[0] not in unigram_rules:
+                            verb_tense_errors += 1
+                            # print(tag, 'not present in unigram')
+                            # print(pos_tagged_sentence)
+                    # Based on length of tag_sequence -  check in the rules of length
+                    tag_sequence = []
+                    if tag[1] == 'TO':
+                        tag_sequence.append(tag[1])
+        # print(tag_sequence)
+        # print("--------------------------------")
+
+    return verb_tense_errors
