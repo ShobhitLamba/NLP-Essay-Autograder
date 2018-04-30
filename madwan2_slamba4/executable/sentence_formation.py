600d0424bd07a8c111f00000d9b9ad7245ab1a91
Mohit Adwani <mohitadwani@gmail.com>
2018-04-30 18:31:54 -0500

    commenting done on sent_form verb_tense sent.py

--- madwan2_slamba4/executable/sentence_formation.py
+++ madwan2_slamba4/executable/sentence_formation.py
@@ -22,17 +22,19 @@ def check_sentence_formation(dot_processed_sentences, nlp):
         parse_tree = parse_tree_string.split()
         line_parse_tree = parse_tree_string.split('\n')
         for item_index, item in enumerate(line_parse_tree):
-            line_parse_tree[item_index] = item.strip()
+            line_parse_tree[item_index] = item.strip() # removes trailing and preceding spaces
 
         if '(ROOT' in parse_tree:
             temp_index = parse_tree.index('(ROOT')
             try:
+                ''' correct Rule - (ROOT (S '''
                 if '(S' not in parse_tree[temp_index + 1]:
                     # print('1. -------------',parse_tree)
                     # print(sentence)
                     # print('*******************************')
                     num_errors += 1
                     continue
+                # ''' incorrect Rule - (ROOT (SBAR '''
                 elif parse_tree[temp_index + 1] == '(SBAR':
                     # print('2. -------------',parse_tree)
                     # print(sentence)
@@ -40,6 +42,7 @@ def check_sentence_formation(dot_processed_sentences, nlp):
                     num_errors += 1
                     continue
                 elif parse_tree[temp_index + 1] == '(SINV':
+                    ''' incorrect Rule - (ROOT (SINV starts with (V'''
                     if '(V' in parse_tree[temp_index + 2]:
                         # print('3. -------------',parse_tree)
                         # print(sentence)
@@ -48,6 +51,7 @@ def check_sentence_formation(dot_processed_sentences, nlp):
                         continue
             except:
                 pass
+        ''' FRAG should not be present '''
         if '(FRAG' in parse_tree:
             # print('4. -------------',parse_tree)
             # print(sentence)
@@ -57,6 +61,7 @@ def check_sentence_formation(dot_processed_sentences, nlp):
         if "(SBAR" in line_parse_tree:
             temp_index = line_parse_tree.index('(SBAR')
             try:
+                ''' correct rule - (NP/(VP (SBAR (S '''
                 if '(S' in line_parse_tree[temp_index + 1]:
                     if '(NP' not in line_parse_tree[temp_index - 1] and '(VP' not in line_parse_tree[temp_index - 1]:
                         # print('5. -------------',line_parse_tree)
